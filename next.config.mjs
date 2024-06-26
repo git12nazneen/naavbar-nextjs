020bfd5ebb72ef2a04d000ece76ac0992347a1a8
git12nazneen <lipinazneen65@gmail.com>
2024-06-27 02:16:55 +0600

    added react drawer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["service64.com"]
+    }
+};
 
 export default nextConfig;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,382 @@
+// "use client";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+
+      
+//   return (
+//     <div>
+//       <nav className="relative bg-[#eff6ff] shadow dark:bg-gray-800">
+//         <div className="container px-2 py-2 mx-auto">
+//           <div className="lg:flex lg:items-center items-center lg:justify-between">
+//             <div className="flex items-center justify-between">
+//               <a href="#">
+//                 <Image
+//                 width={0}
+//                 height={0}
+//                   className="w-auto h-6 sm:h-7"
+//                   src="https://service64.com/static/media/Service64_logo-dark.36b731cc.svg"
+//                   alt="Brand Logo"
+//                 />
+//               </a>
+
+//               {/* search  */}
+
+//               <div className="relative mt-1 md:mt-0 lg:hidden md:hidden">
+//                 <input
+//                   type="text"
+//                   className="w-36 py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-3xl dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+//                   placeholder="Search here..."
+//                 />
+
+//                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+//                   <svg
+//                     className="w-5 h-5 text-gray-400"
+//                     viewBox="0 0 24 24"
+//                     fill="none"
+//                   >
+//                     <path
+//                       d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+//                       stroke="currentColor"
+//                       strokeWidth="2"
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                     ></path>
+//                   </svg>
+//                 </span>
+//               </div>
+
+//               <div className="flex lg:hidden">
+//                 <button
+//                   // onClick={handleOpen}
+//                   onClick={() => setIsOpen(!isOpen)}
+//                   type="button"
+//                   className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+//                   aria-label="toggle menu"
+//                 >
+//                   {!isOpen ? (
+//                     <svg
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       className="w-6 h-6"
+//                       fill="none"
+//                       viewBox="0 0 24 24"
+//                       stroke="currentColor"
+//                       strokeWidth="2"
+//                     >
+//                       <path
+//                         strokeLinecap="round"
+//                         strokeLinejoin="round"
+//                         d="M4 8h16M4 16h16"
+//                       />
+//                     </svg>
+//                   ) : (
+//                     <svg
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       className="w-6 h-6"
+//                       fill="none"
+//                       viewBox="0 0 24 24"
+//                       stroke="currentColor"
+//                       strokeWidth="2"
+//                     >
+//                       <path
+//                         strokeLinecap="round"
+//                         strokeLinejoin="round"
+//                         d="M6 18L18 6M6 6l12 12"
+//                       />
+//                     </svg>
+//                   )}
+//                 </button>
+//               </div>
+             
+            
+//             </div>
+
+//             <div
+//               className={`${
+//                 isOpen
+//                   ? "translate-x-0 opacity-100"
+//                   : "opacity-0 -translate-x-full"
+//               } absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center`}
+//             >
+//               <div className="flex flex-col -mx-6 lg:flex-row lg:items-center items-center lg:mx-8">
+//                 <Link
+//                   href="#"
+//                   className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+//                 >
+//                   Home
+//                 </Link>
+//                 <Link
+//                   href="#"
+//                   className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+//                 >
+//                   Service
+//                 </Link>
+//                 <Link
+//                   href="#"
+//                   className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+//                 >
+//                   Guide
+//                 </Link>
+//                 <Link
+//                   href="#"
+//                   className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+//                 >
+//                   Provider
+//                 </Link>
+//               </div>
+
+//               {/* search  */}
+
+//               <div className="relative mt-4 md:mt-0 lg:block hidden md:hidden">
+//                 <input
+//                   type="text"
+//                   className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-3xl dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+//                   placeholder="Search here..."
+//                 />
+
+//                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+//                   <svg
+//                     className="w-5 h-5 text-gray-400"
+//                     viewBox="0 0 24 24"
+//                     fill="none"
+//                   >
+//                     <path
+//                       d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+//                       stroke="currentColor"
+//                       strokeWidth="2"
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                     ></path>
+//                   </svg>
+//                 </span>
+//               </div>
+
+//               <div className="flex flex-col items-center ml-1 gap-2 mt-4 lg:mt-0 lg:flex-row">
+//                 <button className="btn bg-slate-200 hover:bg-rose-300 lg:px-5 w-full lg:w-20  py-3 rounded-3xl">
+//                   Login
+//                 </button>
+//                 <button className="btn bg-[#5f4bdb] shadow-xl text-white lg:px-5 w-full lg:w-24 py-3 rounded-3xl">
+//                   Sign up
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <div>
+      <nav className="relative bg-[#eff6ff] shadow dark:bg-gray-800">
+        <div className="container px-2 py-2 mx-auto">
+          <div className="lg:flex lg:items-center items-center lg:justify-between">
+            <div className="flex items-center justify-between">
+              <a href="#">
+                <Image
+                  width={0}
+                  height={0}
+                  className="w-auto h-6 sm:h-7"
+                  src="https://service64.com/static/media/Service64_logo-dark.36b731cc.svg"
+                  alt="Brand Logo"
+                />
+              </a>
+
+              <div className="relative mt-1 md:mt-0 lg:hidden md:hidden">
+                <input
+                  type="text"
+                  className="w-36 py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-3xl dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  placeholder="Search here..."
+                />
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+              </div>
+
+              <div className="flex lg:hidden">
+                <button
+                  onClick={toggleDrawer}
+                  type="button"
+                  className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                  aria-label="toggle menu"
+                >
+                  {!isOpen ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 8h16M4 16h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div className="hidden lg:flex lg:items-center items-center lg:relative">
+              <div className="flex flex-col -mx-6 lg:flex-row lg:items-center items-center lg:mx-8">
+                <Link
+                  href="#"
+                  className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="#"
+                  className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+                >
+                  Service
+                </Link>
+                <Link
+                  href="#"
+                  className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+                >
+                  Guide
+                </Link>
+                <Link
+                  href="#"
+                  className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+                >
+                  Provider
+                </Link>
+              </div>
+
+              <div className="relative mt-4 md:mt-0 lg:block hidden md:hidden">
+                <input
+                  type="text"
+                  className="w-full py-2 pl-4 pr-4 text-gray-700 bg-white border rounded-3xl dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  placeholder="Search here..."
+                />
+                {/* absolute z-30 top-1/2 -translate-y-1/2 right-3 text-xl text-slate-600 */}
+                <span className="absolute inset-y-0 right-3 flex items-center pl-3">
+                {/* <span className="absolute inset-y-0 left-0 flex items-center pl-3"> */}
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+              </div>
+
+              <div className="flex flex-col items-center ml-1 gap-2 mt-4 lg:mt-0 lg:flex-row">
+                <button className="btn bg-[#f0eeff] hover:bg-rose-300 lg:px-5 w-full lg:w-20 py-3 rounded-3xl">
+                  Login
+                </button>
+                <button className="btn bg-[#5f4bdb] shadow-xl text-white lg:px-5 w-full lg:w-24 py-3 rounded-3xl">
+                  Sign up
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <Drawer open={isOpen} onClose={toggleDrawer} direction="left" className="bla bla bla">
+        <div className="flex flex-col p-4 items-center">
+          <Link
+            href="#"
+            className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+            // onClick={toggleDrawer}
+          >
+            Home
+          </Link>
+          <Link
+            href="#"
+            className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+            // onClick={toggleDrawer}
+          >
+            Service
+          </Link>
+          <Link
+            href="#"
+            className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+            // onClick={toggleDrawer}
+          >
+            Guide
+          </Link>
+          <Link
+            href="#"
+            className="px-2 py-2 mx-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 text-sm"
+            // onClick={toggleDrawer}
+          >
+            Provider
+          </Link>
+          
+          <div className="flex flex-col w-full items-center ml-1 gap-2 mt-4 lg:mt-0 lg:flex-row">
+                 <button className="btn bg-[#f0eeff] hover:bg-rose-300 lg:px-5 w-full lg:w-20  py-3 rounded-3xl">
+                   Login
+                 </button>
+                 <button className="btn bg-[#5f4bdb] shadow-xl text-white lg:px-5 w-full lg:w-24 py-3 rounded-3xl">
+                   Sign up
+                 </button>
+               </div>
+        </div>
+      </Drawer>
+    </div>
+  );
+};
+
+export default Navbar;
